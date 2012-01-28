0e64cf1efb3dce5ea1740f01eed14e6a3f200fb1
victor-dumitrescu <vichvymetal@gmail.com>
2012-01-28 01:25:01 +0000

    Edited HelloWorld

--- Anti-Tank/src/Game/HelloWorld.java
+++ Anti-Tank/src/Game/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("2nd Line");
+		System.out.println("another line");
 	}
 
 }
