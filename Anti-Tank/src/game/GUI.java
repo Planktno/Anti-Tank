9dc1d2d265ae30580749f02e3f4b6969dcda119b
Victor Dumitrescu <vichvymetal@gmail.com>
2012-03-24 21:21:13 +0000

    Pretty much finished the GUI. Tell me if there's anything more.

--- Anti-Tank/src/game/GUI.java
+++ Anti-Tank/src/game/GUI.java
@@ -4,7 +4,8 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
-import org.newdawn.slick.SlickException;
+//import org.newdawn.slick.SlickException;
+//import org.newdawn.slick.geom.Vector2f;
 import org.newdawn.slick.state.StateBasedGame;
 
 import java.awt.Dimension;
@@ -14,7 +15,7 @@ import states.GameState;
 import entities.Camera;
 import entities.Player;
 import entities.Tank;
-import entities.Weapon;
+//import entities.Weapon;
 import entities.World;
 public class GUI {
 	
@@ -61,22 +62,24 @@ public class GUI {
 		this.world = world;
 	}
 	
-	private void drawTwoDigitNumber(int n, float x, float y, float scale){
+	private void drawNumber(int n, float x, float y, float scale){
+		if (n<100) {
 		int c1=n/10, c2=n%10;
-		//if (c1!=0) {
+			Digit[c1].draw(x,y,scale);
+			Digit[c2].draw(x+12*scale,y,scale);}
+		else {
+		int c1=n/100; int c2=(n-100)/10; int c3=(n-100)%10;
 			Digit[c1].draw(x,y,scale);
 			Digit[c2].draw(x+12*scale,y,scale);
-		//}
-		/*else {
-			Digit[c2].draw(x+6*scale,y,scale);
-		}*/
+			Digit[c3].draw(x+24*scale,y,scale);}
+		
 	}
 	
 	private void formatTime(long time, float x, float y, float scale){
 		int n1=(int)time/60;
 		int n2=(int)time%60;
-		drawTwoDigitNumber(n1, x, y, scale);
-		drawTwoDigitNumber(n2, x+40*scale, y, scale);
+		drawNumber(n1, x, y, scale);
+		drawNumber(n2, x+40*scale, y, scale);
 	}
 	
 	public void render(GameContainer gc, StateBasedGame game, Graphics gr, GameState gs) {		
@@ -160,11 +163,11 @@ public class GUI {
 		int rounds = gs.getRoundsPlayed();
 		Image Round = sprites.getSubImage(429, 106, 65, 20);
 		Round.draw(screenX-controlX+controlX/3+controlX/60, controlY/30, (float)(scale));
-		drawTwoDigitNumber(rounds+1, screenX-controlX/5, controlY/30, (float)(scale));
+		drawNumber(rounds+1, screenX-controlX/5, controlY/30, (float)(scale));
 
 		//Draw the current time
 		//long time = gc.getTime()-gs.getTimeStarted();
-		long time = gc.getTime()/1000 - gs.getTimeStarted()/1000000000;
+		long time = gc.getTime()/1000 - gs.getTimeStarted()/1000000000+1;
 		Image Colon = sprites.getSubImage(565, 109, 6, 19);
 		formatTime(time+1,screenX-controlX/2-controlX/19,controlY/6+controlY/30,scale);
 		Colon.draw(screenX-controlX/2-controlX/19+30*scale,controlY/6+controlY/30, scale);
@@ -176,7 +179,7 @@ public class GUI {
 		float wind = world.getWindSpeed();
 		float windAngle = world.getWindAngle();
 		
-		//load windspeed indicators
+		//load wind speed indicators
 		Image[] windRight = new Image[] {
 			sprites.getSubImage(0, 0, 22, 58),
 			sprites.getSubImage(24, 0, 28, 58),
@@ -258,39 +261,137 @@ public class GUI {
 		if (downPressed == true)
 			strengthDownP.draw(screenX-controlX+controlX/14+controlX/50, controlY-controlY/3+controlY/18, (float)(scale));
 		
-		//Draw the weapon(selection) + Ammo
+		//Draw the weapon(selection)
+				
+		//Weapon[] weapons = currentTank.getWeapons();
+		int currentWeapon = currentTank.getCurrentWeapon();
+		Image[] projectiles = new Image[] {
+				sprites.getSubImage(169, 316, 42, 35),
+				sprites.getSubImage(224, 316, 42, 35)};
+		projectiles[currentWeapon].draw(screenX-controlX+controlX/27, controlY-controlY/6, scale);
+		
+		//draw movement meter
+		Image movementPanel = sprites.getSubImage(166, 247, 105, 38);
+		Image movementFull = sprites.getSubImage(166, 288, 88, 14);
+		
+		float mov = players[currentPlayer].getCurrentTank().getMovementAmount();
+		movementPanel.draw(screenX-2*controlX/3-controlX/50, controlY, scale);
+		movementFull = movementFull.getSubImage(0, 0, (int)(88*(mov/400)), 14);
+		movementFull.draw(screenX-controlX/2-controlX/8, controlY+controlY/20-scale, scale);
+		
 		Image selectWeapon = sprites.getSubImage(62, 206, 22, 19);
 		Image selectWeaponP = sprites.getSubImage(86, 206, 22, 19);
 		selectWeapon.draw(screenX-controlX+controlX/14+controlX/50, controlY-controlY/30, (float)(scale));
+		int mouseX = input.getMouseX();
+		int mouseY = input.getMouseY();
 		
-		Weapon[] weapons = currentTank.getWeapons();
-		int currentWeapon = currentTank.getCurrentWeapon();
-		
-	}
-	
-	
-
-}
-
-
-/*Input input = gc.getInput();
-int mouseX = input.getMouseX();
-int mouseY = input.getMouseY();
-
-//if inside the right Y range for all buttons
-if(mouseY >= camera.getOffset().getY()+9 && mouseY <= camera.getOffset().getY()+9+142-120) {
-	//if inside the right X range for the start button
-	if(mouseX >= camera.getScreenWidth()-152+16 && mouseX <= camera.getScreenWidth()-152+16+84-61) {
-		strengthUp = strengthUpP.copy();//.setRotation(5.0f);
-		if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
-			if (players[currentPlayer].getCurrentTank().getLaunchSpeed() <1)
-				players[currentPlayer].getCurrentTank().setLaunchSpeed( (float)
-						(players[currentPlayer].getCurrentTank().getLaunchSpeed() + 0.005));
-				//players[currentPlayer].getCurrentTank().setLaunchSpeed() += 0.005;
+		//change weapon by mouse
+		if (mouseX > screenX-controlX+controlX/14+controlX/50 &&
+			mouseX < screenX-controlX+controlX/14+controlX/50+selectWeapon.getWidth()*scale &&
+			mouseY > controlY-controlY/30 &&
+			mouseY < controlY-controlY/30+selectWeapon.getHeight()*scale)
+				selectWeaponP.draw(screenX-controlX+controlX/14+controlX/50, controlY-controlY/30, (float)(scale));
+		if (mouseX > screenX-controlX+controlX/14+controlX/50 &&
+			mouseX < screenX-controlX+controlX/14+controlX/50+selectWeapon.getWidth()*scale &&
+			mouseY > controlY-controlY/30 &&
+			mouseY < controlY-controlY/30+selectWeapon.getHeight()*scale &&
+			input.isMousePressed(0)) {
+				
+			if (currentWeapon==0)
+				players[currentPlayer].getCurrentTank().setCurrentWeapon(1);
+			else
+				players[currentPlayer].getCurrentTank().setCurrentWeapon(0);
 		}
-	} else {
-		strengthUp = sprites.getSubImage(61, 122, 84-61, 142-122);
+		
+		//change power by mouse UP
+		if (mouseX > screenX-controlX+controlX/14+controlX/50 &&
+			mouseX < screenX-controlX+controlX/14+controlX/50+strengthUp.getWidth()*scale &&
+			mouseY > controlY/30 &&
+			mouseY < controlY/30+strengthUp.getHeight()*scale)
+					strengthUpP.draw(screenX-controlX+controlX/14+controlX/50, controlY/30, (float)(scale));
+		if (mouseX > screenX-controlX+controlX/14+controlX/50 &&
+			mouseX < screenX-controlX+controlX/14+controlX/50+strengthUp.getWidth()*scale &&
+			mouseY > controlY/30 &&
+			mouseY < controlY/30+strengthUp.getHeight()*scale &&
+			input.isMousePressed(0))
+			if (players[currentPlayer].getCurrentTank().getLaunchSpeed() >= 1)
+				players[currentPlayer].getCurrentTank().setLaunchSpeed(1);
+			else
+				players[currentPlayer].getCurrentTank().setLaunchSpeed(
+						(float)(players[currentPlayer].getCurrentTank().getLaunchSpeed()+0.02));
+		
+		//change power by mouse DOWN
+		if (mouseX > screenX-controlX+controlX/14+controlX/50 &&
+			mouseX < screenX-controlX+controlX/14+controlX/50+strengthDown.getWidth()*scale &&
+			mouseY > controlY-controlY/3+controlY/18 &&
+			mouseY < controlY-controlY/3+controlY/18+strengthDown.getHeight()*scale)
+				strengthDownP.draw(screenX-controlX+controlX/14+controlX/50, controlY-controlY/3+controlY/18, (float)(scale));
+		if (mouseX > screenX-controlX+controlX/14+controlX/50 &&
+			mouseX < screenX-controlX+controlX/14+controlX/50+strengthDown.getWidth()*scale &&
+			mouseY > controlY-controlY/3+controlY/18 &&
+			mouseY < controlY-controlY/3+controlY/18+strengthDown.getHeight()*scale &&
+			input.isMousePressed(0))
+			if (players[currentPlayer].getCurrentTank().getLaunchSpeed() <= 0)
+				players[currentPlayer].getCurrentTank().setLaunchSpeed(0);
+			else
+				players[currentPlayer].getCurrentTank().setLaunchSpeed((float)(players[currentPlayer].getCurrentTank().getLaunchSpeed()-0.02));
+		
+		//change angle by mouse RIGHT
+		if (mouseX > screenX-controlX/6 &&
+			mouseX < screenX-controlX/6+angleRight.getWidth()*scale &&
+			mouseY > controlY/2+controlY/30 &&
+			mouseY < controlY/2+controlY/30+angleRight.getHeight()*scale)
+				angleRightP.draw(screenX-controlX/6, controlY/2+controlY/30, scale);
+		if (mouseX > screenX-controlX/6 &&
+			mouseX < screenX-controlX/6+angleRight.getWidth()*scale &&
+			mouseY > controlY/2+controlY/30 &&
+			mouseY < controlY/2+controlY/30+angleRight.getHeight()*scale &&
+			input.isMousePressed(0)){
+				players[currentPlayer].getCurrentTank().setbAngle(players[currentPlayer].getCurrentTank().getbAngle()+4);
+				if (players[currentPlayer].getCurrentTank().getbAngle() >= 360) 
+					players[currentPlayer].getCurrentTank().setbAngle(players[currentPlayer].getCurrentTank().getbAngle()-360);
+				if (players[currentPlayer].getCurrentTank().getbAngle() <=4)
+					players[currentPlayer].getCurrentTank().setbAngle(0);
+		}
+		
+		//change angle by mouse LEFT
+		if (mouseX > screenX-controlX/2-controlX/8 &&
+			mouseX < screenX-controlX/2-controlX/8+angleLeft.getWidth()*scale &&
+			mouseY > controlY/2+controlY/30 &&
+			mouseY < controlY/2+controlY/30+angleLeft.getHeight()*scale)
+				angleLeftP.draw(screenX-controlX/2-controlX/8, controlY/2+controlY/30, scale);
+		if (mouseX > screenX-controlX/2-controlX/8 &&
+			mouseX < screenX-controlX/2-controlX/8+angleLeft.getWidth()*scale &&
+			mouseY > controlY/2+controlY/30 &&
+			mouseY < controlY/2+controlY/30+angleLeft.getHeight()*scale &&
+			input.isMousePressed(0)) {
+				players[currentPlayer].getCurrentTank().setbAngle(players[currentPlayer].getCurrentTank().getbAngle()-4);
+				if (players[currentPlayer].getCurrentTank().getbAngle() <= 0) 
+					players[currentPlayer].getCurrentTank().setbAngle(players[currentPlayer].getCurrentTank().getbAngle()+360);
+				if (players[currentPlayer].getCurrentTank().getbAngle() <= 176)
+					players[currentPlayer].getCurrentTank().setbAngle(180);
+		}
+		
+		//draw help box
+		Image helpBox = sprites.getSubImage(312, 155, 160, 39);
+		if (time>=1 && time<=3)
+			helpBox.draw(screenX-controlX-controlX/2-controlX/5-controlX/18, controlY+controlY/20, scale);
+	
+		
+		
+////////////////////////////////////////-------------------------------
+	//draw health points
+	
+	//Vector2f relPos = camera.getRelFocusPos(new Vector2f(0,0));
+	//for(int i = 0; i < players.length; i++) {
+	//	for (int j=0; j< players[i].getTanks().length; j++)
+	//	{
+	//		if (players[i].getTank(j).isAlive())
+	//			//drawTwoDigitNumber(players[i].getTank(j).returnHp(),(relPos.getX()/*+players[i].getTank(j).getPos().getX())*camera.getMultipliedScale()*/, (relPos.getY()/*+players[i].getTank(j).getPos().getY())*camera.getMultipliedScale()*/, scale);
+	//			drawNumber(players[i].getTank(j).returnHp(),relPos.getX()+players[i].getTank(j).getPos().getX(), relPos.getY()+players[i].getTank(j).getPos().getY(), 1);//camera.getMultipliedScale());
+	//			//drawString(players[i].getTank(j).returnHp()+"", players[i].getTank(j).getPos().getX()*scale, players[i].getTank(j).getPos().getY()*scale);
+	//	}
+	//}
+		
 	}
-} else {
-	strengthUp = sprites.getSubImage(61, 122, 84-61, 142-122);
-}	*/
+}
